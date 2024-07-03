d6879e45e6794413a5d1a51469945a46de090436
tamaravujic <165172704+tamaravujic@users.noreply.github.com>
2024-07-03 15:14:28 +0100

    Add files via upload
    Used for lec specificity heatmap using ATAC data

---
+++ submit_dbaCount.sh
@@ -0,0 +1,21 @@
+#PBS -l walltime=03:00:00
+#PBS -l select=1:mem=120gb:ncpus=16
+
+cd /rds/general/user/tv722/projects/erg_lymphangiogenesis_project/live/heatmap
+
+module load anaconda3/personal
+source activate diffbind
+
+Rscript - <<EOF
+library(DiffBind)
+library(tidyverse)
+#library(ComplexHeatmap)
+library(ggplot2)
+library(GenomicRanges)
+
+load(file='dbObj')
+load(file='peaks')
+dbObj.counted <- dba.count(dbObj, bUseSummarizeOverlaps=TRUE,summits=100,peaks=peaks, bParallel=TRUE) #DBA_NORM_DEFAULT
+
+save(dbObj.counted, file='dbObj_with_TV_CREs.counted')
+EOF
